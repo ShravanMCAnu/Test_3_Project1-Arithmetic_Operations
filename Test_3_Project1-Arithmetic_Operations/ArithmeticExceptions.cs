b9b20af8300e621c652173a885f300af5d44ece6
ShravanMCAnu <bskmc850@gmail.com>
2022-11-19 16:01:17 +0530

    Add project files.

---
+++ Test_3_Project1-Arithmetic_Operations/ArithmeticExceptions.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_3_Project1_Arithmetic_Operations
+{
+
+    public class ArithmeticExceptions
+    {
+        public static void Arithmetic1(int _number1, int _number2)
+        {
+            int  number3;
+            number3 = _number1 / _number2;
+            Console.WriteLine(number3);
+        }
+        
+        public void Arithmetic2(int _number4)
+        {
+            int number5 = _number4 * _number4 * _number4;
+            Console.WriteLine(number5);
+        }
+
+    }
+    
+}
---
+++ Test_3_Project1-Arithmetic_Operations/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test_3_Project1_Arithmetic_Operations;
+try
+{
+    ArithmeticExceptions.Arithmetic1(10, 0);
+    Console.WriteLine("Enter a value <");
+    int n = int.Parse(Console.ReadLine());
+
+}
+catch(DivideByZeroException d)
+{
+    Console.WriteLine(d.Message);
+}
+catch(Exception e)
+{
+    Console.WriteLine("Arithematic Exception");
+
+}
+finally
+{
+    Console.WriteLine("Finally block");
+}
